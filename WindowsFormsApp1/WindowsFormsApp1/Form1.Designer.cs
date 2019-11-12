6b40dad3902bed9113d641802e55d293048bb0e1
Naghi <noori.naqeeb@gmail.com>
2019-11-11 19:30:52 -0500

    .netquiz2 commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.register = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.fname = new System.Windows.Forms.TextBox();
+            this.lname = new System.Windows.Forms.TextBox();
+            this.tel = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(273, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clear Form";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // register
+            // 
+            this.register.Location = new System.Drawing.Point(405, 275);
+            this.register.Name = "register";
+            this.register.Size = new System.Drawing.Size(126, 37);
+            this.register.TabIndex = 0;
+            this.register.Text = "Register";
+            this.register.UseVisualStyleBackColor = true;
+            this.register.Click += new System.EventHandler(this.register_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(405, 318);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(126, 37);
+            this.btnDelete.TabIndex = 0;
+            this.btnDelete.Text = "Delete users";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(273, 318);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 37);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Count of users";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // fname
+            // 
+            this.fname.Location = new System.Drawing.Point(381, 81);
+            this.fname.Multiline = true;
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(244, 24);
+            this.fname.TabIndex = 1;
+            // 
+            // lname
+            // 
+            this.lname.Location = new System.Drawing.Point(381, 138);
+            this.lname.Multiline = true;
+            this.lname.Name = "lname";
+            this.lname.Size = new System.Drawing.Size(244, 24);
+            this.lname.TabIndex = 1;
+            // 
+            // tel
+            // 
+            this.tel.Location = new System.Drawing.Point(381, 191);
+            this.tel.Multiline = true;
+            this.tel.Name = "tel";
+            this.tel.Size = new System.Drawing.Size(244, 24);
+            this.tel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(169, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "First  name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(169, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last  name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(169, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telephone number";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tel);
+            this.Controls.Add(this.lname);
+            this.Controls.Add(this.fname);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.register);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button register;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox fname;
+        private System.Windows.Forms.TextBox lname;
+        private System.Windows.Forms.TextBox tel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void register_Click(object sender, EventArgs e)
+        {
+            
+            String firstname = fname.Text;
+            String lastname= lname.Text;
+            String phone = tel.Text;
+
+                MessageBox.Show(firstname + " " + lastname + " " + phone);
+
+                FileStream myFile = new FileStream("C:\\Users\\0836092\\Desktop\\users.txt", FileMode.Append);
+                using (StreamWriter writer = new StreamWriter(myFile))
+                {
+                    writer.Write(firstname +"\t");
+                    writer.Write(lastname + "\t");
+                    writer.Write(phone + "\t"+"\n");
+                }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fname.Text = string.Empty;
+            lname.Text = string.Empty;
+            tel.Text = string.Empty;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //var lineCounter = 0L;
+            //using (var reader = new StreamReader(myfile.fname))
+            //{
+            //    while (reader.ReadLine() != null)
+            //    {
+            //        lineCounter++;
+            //    }
+            //    MessageBox.Show (lineCounter);
+            //}
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+        //    File.Delete(myFile);
+        }
+    }
+}
